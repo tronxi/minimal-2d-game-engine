9f13332e698d438093870e02d4eab50a37928edc
tronxi <sergio.gsanchez97@gmail.com>
2024-08-28 00:11:31 +0200

    add engine api

---
+++ minimal-2d-game-engine-api/src/main/java/dev/tronxi/minimal2dgameengineapi/Minimal2dGameEngineApiApplication.java
@@ -0,0 +1,13 @@
+package dev.tronxi.minimal2dgameengineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minimal2dGameEngineApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Minimal2dGameEngineApiApplication.class, args);
+  }
+
+}
---
+++ minimal-2d-game-engine-api/src/test/java/dev/tronxi/minimal2dgameengineapi/Minimal2dGameEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tronxi.minimal2dgameengineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minimal2dGameEngineApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
