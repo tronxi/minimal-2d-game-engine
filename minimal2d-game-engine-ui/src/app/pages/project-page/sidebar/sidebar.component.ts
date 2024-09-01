8bec6b24eeb702b2c2687ad2e5b1c010d25d8d8e
tronxi <sergio.gsanchez97@gmail.com>
2024-09-02 00:25:13 +0200

    add resouces buttons

--- minimal2d-game-engine-ui/src/app/pages/project-page/sidebar/sidebar.component.ts
+++ minimal2d-game-engine-ui/src/app/pages/project-page/sidebar/sidebar.component.ts
@@ -1,16 +1,18 @@
 import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {ProjectResources} from "../../../models/projectResources";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {Level} from "../../../models/level";
 import {ElementClass} from "../../../models/elementclass";
 import {ProjectStateService} from "../../../state/project-state.service";
 import {SelectedEvent} from "../../../state/SelectedEvent";
+import {MatFabAnchor} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatTabGroup, MatTab, NgForOf],
+  imports: [MatTabGroup, MatTab, NgForOf, MatFabAnchor, NgIf, MatIcon],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
@@ -32,4 +34,11 @@ export class SidebarComponent implements OnChanges {
     this.projectStateService.update(SelectedEvent.fromElementClass(elementClass));
   }
 
+  onClickAddLevel() {
+    console.log("add level")
+  }
+
+  onClickAddElement() {
+    console.log("add element")
+  }
 }
