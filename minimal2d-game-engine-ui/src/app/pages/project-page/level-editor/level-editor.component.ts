3a8130665ed9e4fd434c1172179e42fac8b8ff00
tronxi <sergio.gsanchez97@gmail.com>
2024-09-02 01:10:56 +0200

    save level

--- minimal2d-game-engine-ui/src/app/pages/project-page/level-editor/level-editor.component.ts
+++ minimal2d-game-engine-ui/src/app/pages/project-page/level-editor/level-editor.component.ts
@@ -1,23 +1,44 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {Level} from "../../../models/level";
 import {NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {MatFabAnchor} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {Project} from "../../../models/project";
+import {ProjectService} from "../../../services/project.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-level-editor',
   standalone: true,
-  imports: [MatLabel, MatFormField, MatInput, NgIf, FormsModule],
+  imports: [MatLabel, MatFormField, MatInput, NgIf, FormsModule, MatFabAnchor, MatIcon],
   templateUrl: './level-editor.component.html',
   styleUrl: './level-editor.component.css'
 })
 export class LevelEditorComponent {
 
   @Input() level!: Level;
+  @Input() project!: Project;
+  private snackBar = inject(MatSnackBar);
 
-  constructor() {
+  constructor(private projectService: ProjectService) {
 
   }
 
+  onClickSaveLevel() {
+    this.projectService.addLevel(this.project, this.level).subscribe({
+      next: _ => {
+        this.snackBar.open("Level saved successfully!", "", {
+          duration: 3000
+        });
+      },
+      error: _ => {
+        this.snackBar.open("Error saving level", "", {
+          duration: 5000
+        });
+      }
+    });
+  }
 }
